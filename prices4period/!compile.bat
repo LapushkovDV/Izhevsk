3125e5e4136cf4e086145b33c1cdb306c039ea71
lapushkov <lapushkov@yandex.ru>
2026-03-14 15:58:31 +0300

    prices4period_recalc

---
+++ prices4period/!compile.bat
@@ -0,0 +1,26 @@
+rem это год-месяц-день YYYYMMDD для WINXP
+@set DD=%DATE:~6,4%%DATE:~3,2%%DATE:~0,2%
+
+del *.res
+del *.tmp
+del *.crf
+del *.tmp
+del *.dsk
+del *.fnc
+del *.log
+del Atlantis*.res
+
+@set exedir=C:\Galaktika\AVAZ\chumachenko\6.0.09.0\exe
+
+@%exedir%\vip64x.exe prices4period.prj /r:prices4period.res /c:vip.cfg /u:SUPERVISOR /#user:SUPERVISOR
+rem /def:Atl60
+
+rem /c:support_res.cfg
+@echo Компиляция завершена
+@goto END
+
+:END
+
+pause
+@exit
+
